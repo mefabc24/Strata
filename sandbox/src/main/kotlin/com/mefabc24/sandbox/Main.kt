a5d76e284ecaf57d307b78900eb506aa003cbc99
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-09-18 15:54:10 +0200

    fix(sandbox): update game title from "City Builder" to "Sandbox"

--- sandbox/src/main/kotlin/com/mefabc24/sandbox/Main.kt
+++ sandbox/src/main/kotlin/com/mefabc24/sandbox/Main.kt
@@ -5,7 +5,7 @@ import com.mefabc24.strata.desktop.DesktopLauncher
 fun main() {
     DesktopLauncher.launch(
         game = SandboxGame(),
-        title = "City Builder",
+        title = "Sandbox",
         width = 1280,
         height = 720
     )
