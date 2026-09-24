53c875c8470f6659a609eb65bc2ff92c07f1dbb4
mefabc24 <fabian.mecke@student.reutlingen-university.de>
2026-09-24 16:55:32 +0200

    fix(engine): normalize momentary button state

--- engine/core/src/main/kotlin/com/mefabc24/strata/ui/StrataWidgets.kt
+++ engine/core/src/main/kotlin/com/mefabc24/strata/ui/StrataWidgets.kt
@@ -3,6 +3,7 @@ package com.mefabc24.strata.ui
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.InputListener
+import com.badlogic.gdx.scenes.scene2d.ui.Button
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
@@ -29,6 +30,7 @@ class StrataButton(
 
     init {
         setProgrammaticChangeEvents(false)
+        preventCheckedState()
 
         addListener(
             object : ClickListener() {
@@ -39,12 +41,25 @@ class StrataButton(
                 ) {
                     if (isDisabled) return
 
-                    isChecked = false
                     this@StrataButton.onClick()
                 }
             }
         )
     }
+}
+
+private fun Button.preventCheckedState() {
+    addCaptureListener(
+        object : ChangeListener() {
+            override fun changed(
+                event: ChangeEvent,
+                actor: Actor
+            ) {
+                event.cancel()
+                event.stop()
+            }
+        }
+    )
 }
 
 /**
